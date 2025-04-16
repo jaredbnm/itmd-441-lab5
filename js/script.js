ed7ea337c4aae71a6bec5de00e1fdc00ea060ddf
jaredbnm <jaredjr0204@gmail.com>
2025-04-16 17:34:02 -0500

    finished all requirements

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function calcBillWithTax() {
   let bill = parseFloat(billInput.value);
   let tax = bill * 0.11;
   let billWithTax = bill + tax;
-  billwTax.value = billWithTax.toFixed(2);
+  billwTax.value = `$${billWithTax.toFixed(2)}`;
 }
 
 // getting the tip amount
@@ -28,7 +28,7 @@ function calcTipAmount() {
   let billWithTax = bill + tax;
   let tipPercent = parseFloat(tipRange.value);
   let tip = billWithTax * tipPercent / 100;
-  tipAmount.value = tip.toFixed(2);
+  tipAmount.value = `$${tip.toFixed(2)}`;
 }
 
 // calculate total bill with tip and tax
@@ -39,13 +39,18 @@ function calcTotalBill() {
   let tipPercent = parseFloat(tipRange.value);
   let tip = billWithTax * tipPercent / 100;
   let total = billWithTax + tip;
-  totalBill.value = total.toFixed(2);
+  totalBill.value = `$${total.toFixed(2)}`;
 }
 
 function billValidation() {
   let bill = parseFloat(billInput.value);
   if (isNaN(bill) || bill <= 0) {
     billError.textContent = 'Please enter a valid number.';
+    billwTax.value = '$0.00';
+    tipAmount.value = '$0.00';
+    totalBill.value = '$0.00';
+    tipPercent.value = '0%';
+    tipRange.value = 0;
   } else {
     billError.textContent = '';
   }
